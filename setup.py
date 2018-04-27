57441db43e74bcefb4955464f1a9e4d5dca07092
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-04-27 13:32:53 +0200

    Back to development: 2.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.restapi.pam',
-    version='2.0.0',
+    version='2.0.1.dev0',
     description="An add-on providing plone.restapi endpoint for translations handled using plone.app.multilingual",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
